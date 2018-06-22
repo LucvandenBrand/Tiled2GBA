92fe3825e4b342e856789a0b36e83066b46a2feb
Luc van den Brand <10974297+LucvandenBrand@users.noreply.github.com>
2018-06-22 21:42:16 +0200

    Added cxxoptions.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,60 @@
 #include <iostream>
+#include <fstream>
 #include "lib/tmxlite/Map.hpp"
 #include "main.h"
 #include "lib/tmxlite/TileLayer.hpp"
 #include "tileLayerConverter/tileLayerConverter.h"
 #include "gbaMap/gbaMap.h"
 #include "tileSetConverter/tileSetConverter.h"
+#include "lib/cxxopts/cxxopts.hpp"
 
 int main(int argc, char **argv)
 {
-    if (argc < NUM_ARGS) {
-        cout << "Usage: " << argv[0] << " mapFile.tmx" << endl;
+    cxxopts::Options options(argv[0], "Tiled Map to Gameboy Advance tile map converter");
+
+    options
+            .allow_unrecognised_options()
+            .add_options()
+                    ("h, help", "Print this help screen.")
+                    ("i, input", "TMX map file.", cxxopts::value<string>(), "map.tmx")
+                    ("o, output", "Output paths.", cxxopts::value<vector<string>>(), "files");
+
+    options.parse_positional({"input", "output"});
+
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help")) {
+        cout << options.help() << endl;
+        return EXIT_SUCCESS;
+    }
+
+    if (!result.count("input"))
+    {
+        cout << "Please provide an input file! See --help for more information." << endl;
         return EXIT_FAILURE;
     }
 
-    string mapPath = argv[1];
+    string mapPath = result["input"].as<string>();
+
+    if (!result.count("output")) {
+        cout << "Please provide output information! See --help for more information." << endl;
+        return EXIT_FAILURE;
+    }
+
+    vector<string> outPaths = result["output"].as<vector<string>>();
+
+    if (outPaths.size() < 2) {
+        cout << "Please provide a *.c and a *.h output file. See --help for more information." << endl;
+        return EXIT_FAILURE;
+    }
+
     tmx::Map map;
     if (!map.load(mapPath)) {
-        cout << "[ERR] Problem whilst loading " << mapPath << '!' << endl;
         return EXIT_FAILURE;
     };
 
-    cout << "Loaded " << mapPath << " version " << map.getVersion().upper << "." << map.getVersion().lower << '.' << endl;
+    cout << "Tiled2GBA v0.1 by Luc van den Brand" << endl << endl;
+    cout << "Loaded " << mapPath << " v" << map.getVersion().upper << "." << map.getVersion().lower << '.' << endl;
 
     auto tileSize = map.getTileSize();
     if (tileSize.x != TILE_SIZE || tileSize.y != TILE_SIZE) {
@@ -67,9 +101,17 @@ int main(int argc, char **argv)
         gbaMap->addTileLayer(tileLayerBytes);
     }
 
-    cout << endl << "Results:" << endl;
-    gbaMap->toCode(cout, cout);
+    cout << "Generating code...";
 
-    cout << "Done!" << endl;
+    ofstream codeFile, headerFile;
+    codeFile.open (outPaths[0]);
+    headerFile.open (outPaths[1]);
+    gbaMap->toCode(headerFile, codeFile);
+    codeFile.close();
+    headerFile.close();
+
+    cout << " done!" << endl << endl;
+    cout << "Code stored in " << outPaths[0] << " and " << outPaths[1] << "." << endl;
+
     return EXIT_SUCCESS;
 }
