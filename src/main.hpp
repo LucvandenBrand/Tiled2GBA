79e86e85534068855030e3cff6b1c65ffadb06d0
Luc van den Brand <10974297+LucvandenBrand@users.noreply.github.com>
2018-10-02 13:30:36 +0200

    Increased patch version for intermentiate release

--- src/main.hpp
+++ src/main.hpp
@@ -5,7 +5,7 @@ const char *PROGRAM_NAME = "Tiled2GBA";
 const char *AUTHOR = "Luc van den Brand";
 const int VERSION_MAJOR = 0;
 const int VERSION_MINOR = 3;
-const int VERSION_PATCH = 0;
+const int VERSION_PATCH = 5;
 
 using namespace std;
 
