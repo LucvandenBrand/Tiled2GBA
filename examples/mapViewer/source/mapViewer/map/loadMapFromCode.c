5a4d72a960c1e2a6eea6df5c292e3b25b9aca7b3
Luc van den Brand <10974297+LucvandenBrand@users.noreply.github.com>
2018-10-07 10:16:36 +0200

    Object loading from code implemented (untested strings)

--- examples/mapViewer/source/mapViewer/map/loadMapFromCode.c
+++ examples/mapViewer/source/mapViewer/map/loadMapFromCode.c
@@ -16,5 +16,14 @@ Map loadMapFromCode() {
     map.tileMapLength = mapTileMap0Length;
     map.tileMapLayers[0] = &mapTileMap0[0];
 
+    u16 objectDataindex = 0;
+    u32 objectCount = 0;
+    while (objectDataindex != mapObjectsLength) {
+        Object object = loadObject(mapObjects, &objectDataindex);
+        map.objects[objectCount] = object;
+        objectCount++;
+    }
+    map.numObjects = objectCount;
+
     return map;
 }
