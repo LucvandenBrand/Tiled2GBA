aa25e1c68a425bb4f3958ae7d54cce255ba992af
Luc van den Brand <10974297+LucvandenBrand@users.noreply.github.com>
2018-10-07 15:48:03 +0200

    Fixed broken name style.

--- examples/mapViewer/source/mapViewer/map/loadMapFromCode.c
+++ examples/mapViewer/source/mapViewer/map/loadMapFromCode.c
@@ -16,10 +16,10 @@ Map loadMapFromCode() {
     map.tileMapLength = mapTileMap0Length;
     map.tileMapLayers[0] = &mapTileMap0[0];
 
-    u16 objectDataindex = 0;
+    u16 objectDataIndex = 0;
     u32 objectCount = 0;
-    while (objectDataindex != mapObjectsLength) {
-        Object object = loadObject(mapObjects, &objectDataindex);
+    while (objectDataIndex != mapObjectsLength) {
+        Object object = loadObject(mapObjects, &objectDataIndex);
         map.objects[objectCount] = object;
         objectCount++;
     }
