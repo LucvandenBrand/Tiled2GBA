3630388026e6bbc648037237c853bd113094eae9
Luc van den Brand <10974297+LucvandenBrand@users.noreply.github.com>
2018-10-08 16:52:56 +0200

    Increased minor version to 4.

--- src/main.hpp
+++ src/main.hpp
@@ -4,8 +4,8 @@
 const char *PROGRAM_NAME = "Tiled2GBA";
 const char *AUTHOR = "Luc van den Brand";
 const int VERSION_MAJOR = 0;
-const int VERSION_MINOR = 3;
-const int VERSION_PATCH = 5;
+const int VERSION_MINOR = 4;
+const int VERSION_PATCH = 0;
 
 using namespace std;
 
